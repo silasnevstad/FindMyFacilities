5b9d3882a21e8ac04e4b4e3ead5c1de231f666d3
Silas Nevstad <nevstads@gmail.com>
2024-11-29 18:13:07 -0500

    Initial Commit

---
+++ find-my-facilities/find_my_facilitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  find_my_facilitiesApp.swift
+//  find-my-facilities
+//
+//  Created by Silas Nevstad on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct find_my_facilitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ find-my-facilitiesTests/find_my_facilitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  find_my_facilitiesTests.swift
+//  find-my-facilitiesTests
+//
+//  Created by Silas Nevstad on 11/29/24.
+//
+
+import Testing
+@testable import find_my_facilities
+
+struct find_my_facilitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
